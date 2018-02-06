486317de88ef1a81e229e4a1c89680a724d83f0f
Mishen <mishen888@yandex.ru>
2018-02-06 20:56:50 +0300

    Commit1

---
+++ Консоль1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Консоль1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[4,2];
+            int increment = 1;
+            for (int index = 0; index < 4; index++)
+            {
+                for (int index2 = 0; index2 < 2; index2++)
+                {
+                    array[index, index2] = increment++;
+                    Console.Write(array[index, index2]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
